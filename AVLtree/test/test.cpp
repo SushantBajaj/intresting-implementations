d79774a30f2d75692dd3048afa66dd28b8f2c121
SushantBajaj <sushant1108bajaj@gmail.com>
2026-06-12 23:45:48 +0530

    add an insertion and deletion test for avl

--- AVLtree/test/test.cpp
+++ AVLtree/test/test.cpp
@@ -1,9 +1,7 @@
-#include "avl_node.h"
 #include "avl_tree.h"
 #include <cstdint>
-#include <iostream>
 #include <random>
-#include <cstdlib>
+#include <vector>
 
 
 uint64_t Random64(){
@@ -12,15 +10,25 @@ uint64_t Random64(){
 }
 
 
+
+
 void test1(){
     AVLtree avt;
+    std::vector<uint64_t> arr;
     for(int i =0;i<1000000;i++){
-        avt.insertNode(Random64());
+        int64_t num = Random64();
+        arr.push_back(num);
+        avt.insertNode(num);
     }
+    for(auto i : arr){
+        if(i%2==0){
+            avt.deleteNode(i);
+        }
+    }
+
     if(avt.verifyBST() && avt.verifyHeightDiff()){
         avt.printTree();
     }
-    
 
 }
 
